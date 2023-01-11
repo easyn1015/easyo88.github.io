91b2a39eb9910ca3547f6ebaea96842c656ab922
easyn1015 <easyo1015@gmail.com>
2023-01-12 00:37:03 +0900

    Fix: mobile

--- src/ControllableParticles.js
+++ src/ControllableParticles.js
@@ -13,7 +13,7 @@ export function ControllableParticles() {
     const props = useControls({
         focus: { value: 5.9, min: 3, max: 7, step: 0.01 },
         speed: { value: 11.8, min: 0.1, max: 100, step: 0.1 },
-        aperture: { value: 5.6, min: 1, max: 5.6, step: 0.1 },
+        aperture: { value: 4.6, min: 1, max: 5.6, step: 0.1 },
         fov: { value: 54, min: 0, max: 200 },
         curl: { value: 0.02, min: 0.01, max: 0.5, step: 0.01 },
     });
