55280613a0dd6f849803b3bc1d422f9aa1b5a994
easyn1015 <easyo1015@gmail.com>
2023-01-15 17:03:31 +0900

    Fix: url

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ export default function App() {
     }, []);
 
     return (
-        <Router basename='/'>
+        <Router basename='/portfolio'>
             {loading ? (
                 <Loading />
             ) : (
